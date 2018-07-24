7e867bcd5bc34ca77c3329e31a63842c85b843ca
Alexey Rakitsky <arakitsky@ecisys.com>
2018-07-24 10:07:55 +0300

    review

--- src/main/java/pages/BaseCatalogProductsPage.java
+++ src/main/java/pages/BaseCatalogProductsPage.java
@@ -5,11 +5,13 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
+//TODO Все Base классы всегда абстрактные
 public class BaseCatalogProductsPage extends BasePage{
     public BaseCatalogProductsPage(By identifyElementLocator, WebDriver d) {
         super(identifyElementLocator, d);
     }
 
+    //TODO Зачем здесь эти параметры? Ты их получаешь в конструкторе, зачем еще и здесь их запрашивать?
     @Override
     public boolean isPageOpened(WebDriver driver, By identifyElementLocator) {
         try {
